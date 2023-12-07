077fc639cae649f05116d5395d7958a9eceeb131
Maksim Lanin <max.m.lanin@gmail.com>
2023-12-08 00:19:25 +0200

    Day7. Consistently use low-to-high ordering

--- puzzles/src/main/scala/Day7.scala
+++ puzzles/src/main/scala/Day7.scala
@@ -5,6 +5,11 @@ import kyo.App.Effects
 import kyo.tries.Tries
 
 object Day7 extends util.AocApp(2023, 7) {
+  given orderFromOrdering[A: Ordering]: cats.kernel.Order[A] = cats.kernel.Order.fromOrdering(implicitly)
+
+  private def reverse[A](using oa: Ordering[A]): Ordering[A] = oa.reverse
+  private def reverse[A](using oa: cats.kernel.Order[A]): cats.kernel.Order[A] = cats.kernel.Order.reverse(oa)
+
   case class Input(handBids: NonEmptyList[HandBid])
 
   case class HandBid(hand: Hand, bid: Long)
@@ -13,14 +18,14 @@ object Day7 extends util.AocApp(2023, 7) {
 //    override def toString: String = s"${cards.toList.mkString("")}:$handType1(${cardsSorted.toList.mkString("")})"
 //    override def toString: String = s"${cards.toList.mkString("")}:$handType2(${cardsSorted2.mkString("")})"
 
-    def cardsSorted = cards.sorted
+    def cardsSorted = cards.sorted(reverse)
     def cardsSorted2 = {
       val (js, rest) = cards.toList.partition(_ == Card.J)
-      js ++ rest.sorted
+      js ++ rest.sorted(reverse)
     }
 
     val handType1: HandType = {
-      cards.toList.groupBy(identity).values.map(_.size).toList.sorted.reverse match {
+      cards.toList.groupBy(identity).values.map(_.size).toList.sorted(reverse) match {
         case 5 :: _      => HandType.`5`
         case 4 :: _      => HandType.`4`
         case 3 :: 2 :: _ => HandType.`3_2`
@@ -33,7 +38,7 @@ object Day7 extends util.AocApp(2023, 7) {
 
     val handType2: HandType = {
       val (js, rest) = cards.toList.partition(_ == Card.J)
-      val cardCounts = rest.groupBy(identity).values.map(_.size).toList.sorted.reverse
+      val cardCounts = rest.groupBy(identity).values.map(_.size).toList.sorted(reverse)
       val adjustedCounts = cardCounts match
         case n :: rest => (n + js.size) :: rest
         case Nil       => js.size :: Nil
@@ -54,23 +59,20 @@ object Day7 extends util.AocApp(2023, 7) {
 
     given ordering1: Ordering[Hand] = Ordering.by(h => (h.handType1, h.cards.toList))
     given ordering2: Ordering[Hand] =
-      Ordering.by(h => (h.handType2, h.cards.map(_.some.filter(_ != Card.J).map(_.ordinal).getOrElse(100)).toList))
-//    given cats.kernel.Order[Hand] = cats.kernel.Order.fromOrdering
+      Ordering.by(h => (h.handType2, h.cards.map(_.some.filter(_ != Card.J).map(_.ordinal).getOrElse(-1)).toList))
   }
 
   enum Card:
-    case A, K, Q, J, T, `9`, `8`, `7`, `6`, `5`, `4`, `3`, `2`
+    case `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, T, J, Q, K, A
 
   object Card:
     given Ordering[Card] = Ordering.by(_.ordinal)
-    given cats.kernel.Order[Card] = cats.kernel.Order.fromOrdering
 
   enum HandType:
-    case `5`, `4`, `3_2`, `3`, `2_2`, `2`, `1`
+    case `1`, `2`, `2_2`, `3`, `3_2`, `4`, `5`
 
   object HandType:
     given Ordering[HandType] = Ordering.by(_.ordinal)
-    given cats.kernel.Order[Card] = cats.kernel.Order.fromOrdering
 
   def parseInput(s: String): Input > Effects = {
     import cats.parse.{Numbers => N, Parser => P, Rfc5234 => R}
@@ -96,8 +98,9 @@ object Day7 extends util.AocApp(2023, 7) {
   def part1(input: Input): String > Effects = {
     val result = input.handBids.toList
       .sortBy(_.hand)(Hand.ordering1)
-      .reverseIterator
+      .iterator
       .zip(Iterator.from(1))
+//      .tapEach(println)
       .map((hb, rank) => hb.bid * rank)
       .sum
     s"$result"
@@ -106,8 +109,9 @@ object Day7 extends util.AocApp(2023, 7) {
   def part2(input: Input): String > Effects = {
     val result = input.handBids.toList
       .sortBy(_.hand)(Hand.ordering2)
-      .reverseIterator
+      .iterator
       .zip(Iterator.from(1))
+//      .tapEach(println)
       .map((hb, rank) => hb.bid * rank)
       .sum
     s"$result"
