3a44a7c9ba8a9c2e489d208c6d4437e14485de63
Maksim Lanin <max.m.lanin@gmail.com>
2023-12-10 20:25:35 +0200

    Rework Day10 (still dirty but has cool bits)
    Squashed commit of the following:
        Rework findPathCoords, implement part2 using geometry
        Implement findPathSeq with unfold
        Don't use mutable arrays
        Add HVDirection
        Move Coord out of CoordSpace

--- puzzles/src/main/scala/Day10.scala
+++ puzzles/src/main/scala/Day10.scala
@@ -9,32 +9,62 @@ import scala.annotation.tailrec
 import scala.collection.immutable.{BitSet, Queue}
 
 object Day10 extends util.AocApp(2023, 10) {
-  case class Input(cs: CoordSpace, cells: Array[Cell]) {
+  case class Input(cs: CoordSpace, cells: IndexedSeq[Cell]) {
     override def toString: String =
       s"$cs\n${cells.iterator.grouped(cs.w).map(_.mkString).mkString("", "\n", "\n")}"
 
-    def s: cs.Coord = cs.coord(cells.indexOf(Cell.`S`))
+    def s: Coord = cs.coord(cells.indexOf(Cell.`S`))
   }
 
-  enum Cell:
+  enum Cell {
     case `S`, `.`, `|`, `-`, `L`, `J`, `7`, `F`
 
-  case class CoordSpace(w: Int, h: Int) {
-    private val size = w * h
+    def adjacentDirections: List[HVDirection] = this match
+      case Cell.`S` => List(HVDirection.U, HVDirection.D, HVDirection.L, HVDirection.R)
+      case Cell.`.` => List.empty
+      case Cell.`|` => List(HVDirection.U, HVDirection.D)
+      case Cell.`-` => List(HVDirection.L, HVDirection.R)
+      case Cell.`L` => List(HVDirection.U, HVDirection.R)
+      case Cell.`J` => List(HVDirection.U, HVDirection.L)
+      case Cell.`7` => List(HVDirection.D, HVDirection.L)
+      case Cell.`F` => List(HVDirection.D, HVDirection.R)
+  }
 
-    opaque type Coord = Int
-
-    def coord(i: Int): Coord = i
-
-    extension (c: Coord) {
-      def i: Int = c
-      def rc: (Int, Int) = (c / w, c % w)
-
-      def u: Option[Coord] = Option.when(c >= w)(c - w)
-      def d: Option[Coord] = Option.when((c + w) < size)(c + w)
-      def l: Option[Coord] = Option.when((c % w) != 0)(c - 1)
-      def r: Option[Coord] = Option.when(((c + 1) % w) != 0)(c + 1)
+  object Cell {
+    def fromOutgoingDirections(dirs: HVDirection*): Option[Cell] = {
+      val ds = dirs.toSet
+      Cell.values.find(_.adjacentDirections.toSet == ds)
     }
+  }
+
+  enum HVDirection {
+    case U, D, L, R
+
+    def inverse: HVDirection = this match {
+      case HVDirection.U => HVDirection.D
+      case HVDirection.D => HVDirection.U
+      case HVDirection.L => HVDirection.R
+      case HVDirection.R => HVDirection.L
+    }
+  }
+
+  case class Coord(cs: CoordSpace, idx: Int) {
+    def r: Int = idx / cs.w
+    def c: Int = idx % cs.w
+    def rc: (Int, Int) = (r, c)
+
+    def next(dir: HVDirection): Option[Coord] = dir match {
+      case HVDirection.U => Option.when(idx > cs.w)(copy(idx = idx - cs.w))
+      case HVDirection.D => Option.when((idx + cs.w) < cs.size)(copy(idx = idx + cs.w))
+      case HVDirection.L => Option.when((idx % cs.w) != 0)(copy(idx = idx - 1))
+      case HVDirection.R => Option.when(((idx + 1) % cs.w) != 0)(copy(idx = idx + 1))
+    }
+  }
+
+  case class CoordSpace(w: Int, h: Int) {
+    val size: Int = w * h
+
+    def coord(i: Int): Coord = Coord(this, i)
   }
 
   def parseInput(s: String): Input > Effects = {
@@ -58,74 +88,71 @@ object Day10 extends util.AocApp(2023, 10) {
       .fold(Tries.fail, identity)
   }
 
-  private def adjacent(cell: Cell, cs: CoordSpace, c: cs.Coord): List[cs.Coord] = cell match {
-    case Cell.`S` => List(c.u, c.d, c.l, c.r).flatten
-    case Cell.`.` => List.empty
-    case Cell.`|` => List(c.u, c.d).flatten
-    case Cell.`-` => List(c.l, c.r).flatten
-    case Cell.`L` => List(c.u, c.r).flatten
-    case Cell.`J` => List(c.u, c.l).flatten
-    case Cell.`7` => List(c.d, c.l).flatten
-    case Cell.`F` => List(c.d, c.r).flatten
-  }
+  private def adjacent(cell: Cell, c: Coord): List[Coord] = cell.adjacentDirections.flatMap(c.next)
 
-  private def mutuallyAdjacent(input: Input, c: input.cs.Coord): List[input.cs.Coord] = {
-    adjacent(input.cells(c.i), input.cs, c).filter(c1 => adjacent(input.cells(c1.i), input.cs, c1).contains(c))
-  }
-
-  private def findPath(input: Input): Option[BitSet] = {
-    @tailrec def rec(c0: input.cs.Coord, c: input.cs.Coord, path: BitSet): Option[BitSet] = {
-      val atCell             = input.cells(c.i)
-      val (visited, toVisit) = mutuallyAdjacent(input, c).partition(c => path(c.i))
-      /*
-      println {
-        val sPath = Iterator
-          .from(0)
-          .map(i =>
-            if (i == c.i) input.cells(c.i).toString
-            else if (path(i)) "#"
-            else input.cs.coord(i).some.filter(toVisit.contains).as(input.cells(i).toString).getOrElse(" "),
-          )
-          .grouped(input.cs.w)
-          .take(input.cs.h)
-          .map(_.mkString)
-          .mkString("\n")
-        val hSep = "-" * input.cs.w
-        val header = s"c0=$c0, c=$c ${ c.rc }, visited=$visited, toVisit=$toVisit, path=${ path.toList.sorted }"
-        s"\n$header\n$hSep$sPath\n$hSep"
-      }
-       */
-      if (visited.exists(_ != c0))
-        (path + c.i).some
-      else {
-        toVisit.headOption match {
-          case Some(c1) => rec(c, c1, path + c.i)
-          case None     => None
-        }
-      }
-    }
-
-    val path0 = BitSet.empty + input.s.i
-    mutuallyAdjacent(input, input.s).collectFirstSome(rec(input.s, _, path0))
+  private def mutuallyAdjacent(input: Input, c: Coord): List[Coord] = {
+    adjacent(input.cells(c.idx), c).filter(c1 => adjacent(input.cells(c1.idx), c1).contains(c))
   }
 
   /*
-  private def countEnclosed(cs: CoordSpace, path: BitSet): Int = {
-    def inRow(r: Int) = Iterator
-      .from(r * cs.w)
-      .take(cs.w)
-      .map(path)
-      .foldLeft((0, false)) {
-        case ((n, false), false) => (n, false)
-        case ((n, false), true)  => (n, true)
-        case ((n, true), false)  => (n + 1, true)
-        case ((n, true), true)   => (n, false)
-      }
-      ._1
+  private def findPathCoordsCells(input: Input): Option[IndexedSeq[(Coord, Cell)]] = {
+    input
+      .cells(input.s.idx)
+      .adjacentDirections
+      .tupleLeft(input.s)
+      .collectFirstSome { (c0, dir0) =>
+        // Build a path of adjacent nodes. A full loop _ends_ with `c0`.
+        // That is to distinct it from an incomplete path.
+        val itPath: Iterator[(Coord, Cell)] = Iterator.unfold((c0, dir0).some) {
+          case None => None
 
-    Iterator.from(0).take(cs.h).map(inRow).sum
+          case Some((c, dir)) =>
+            c.next(dir).flatMap {
+              case `c0` =>
+                // Loop's closed. Replace `c0` with a compatible 2-sided pipe.
+                val cell = Cell.fromOutgoingDirections(dir0, dir.inverse).get
+                ((c0, cell), None).some // Loop's closed
+              case c1 =>
+                val cell = input.cells(c1.idx)
+                val state1 = cell.adjacentDirections
+                  .filter(_ != dir.inverse)
+                  .some
+                  .collectFirst { case List(dir1) => (c1, dir1) }
+                ((c1, cell), state1).some
+            }
+        }
+        itPath.toIndexedSeq.some.filter(_.lastOption.exists(_._1 == c0))
+      }
   }
    */
+
+  private def findPathCoords(input: Input): Option[IndexedSeq[Coord]] = {
+    input
+      .cells(input.s.idx)
+      .adjacentDirections
+      .tupleLeft(input.s)
+      .collectFirstSome { (c0, dir0) =>
+        // Build a path of adjacent nodes. A full loop _ends_ with `c0`.
+        // That is to distinct it from an incomplete path.
+        val itPath = Iterator.unfold((c0, dir0).some) {
+          case None => None
+          case Some((c, dir)) =>
+            c.next(dir).flatMap {
+              case `c0` => (c0, None).some // Loop's closed
+              case c1 =>
+                input.cells(c1.idx).adjacentDirections.filter(_ != dir.inverse) match {
+                  case List(dir1) => (c1, (c1, dir1).some).some
+                  case _          => (c1, None).some // Dead end
+                }
+            }
+        }
+        itPath.toIndexedSeq.some.filter(_.lastOption.contains(c0))
+      }
+  }
+
+  private def findPath(input: Input): Option[BitSet] =
+    findPathCoords(input).map(_.foldLeft(BitSet.empty)(_ + _.idx))
+
   private def enclosed(input: Input, path: BitSet): BitSet = {
     enum VDirection:
       case U, D
@@ -134,7 +161,9 @@ object Day10 extends util.AocApp(2023, 10) {
 
     val sAdj = mutuallyAdjacent(input, input.s)
     val sCell =
-      List(input.s.u, input.s.r, input.s.d, input.s.l).map(_.exists(c => path(c.i) && sAdj.contains(c))) match {
+      List(HVDirection.U, HVDirection.R, HVDirection.D, HVDirection.L)
+        .map(input.s.next)
+        .map(_.exists(c => path(c.idx) && sAdj.contains(c))) match {
         case List(true, true, false, false) => Cell.`L`
         case List(true, false, true, false) => Cell.`|`
         case List(true, false, false, true) => Cell.`J`
@@ -145,8 +174,7 @@ object Day10 extends util.AocApp(2023, 10) {
         case List(false, false, true, true) => Cell.`7`
       }
 
-    val cells = Array.copyOf(input.cells, input.cells.length)
-    cells(input.s.i) = sCell
+    val cells = input.cells.updated(input.s.idx, sCell)
 
     Iterator
       .from(0)
@@ -188,11 +216,35 @@ object Day10 extends util.AocApp(2023, 10) {
   def part1(input: Input): String > Effects = {
 //    println(s"$input")
 
-    val result = findPath(input).map(_.size / 2).getOrElse(-1)
+    val result = findPathCoords(input).map(_.size / 2).getOrElse(-1)
     s"$result"
   }
 
   def part2(input: Input): String > Effects = {
+    part2_shoelacePicks(input)
+  }
+
+  /** Use geometry. Kudos to @jurisk for this. */
+  def part2_shoelacePicks(input: Input): String > Effects = {
+    val result = findPathCoords(input)
+      .map { coords =>
+        // https://en.wikipedia.org/wiki/Shoelace_formula
+        val area = (coords.iterator ++ Iterator(coords.head))
+          .sliding(2)
+          .map { case Seq(c1, c2) => (c1.r + c2.r) * (c1.c - c2.c) }
+          .sum
+          .abs / 2
+        // Now, using Pick's theorem get the number of inner points from area and the number of outer points.
+        // https://en.wikipedia.org/wiki/Pick%27s_theorem
+        val inside = area + 1 - coords.size / 2
+        inside
+      }
+      .getOrElse(-1)
+
+    s"$result"
+  }
+
+  def _part2(input: Input): String > Effects = {
     val result = findPath(input)
 //      .tapEach(p => println(showPath(input.cs, p)))
       .headOption
