f2784a548a4f7f48744639c99e75b57fc9266899
Maksim Lanin <max.m.lanin@gmail.com>
2023-12-21 18:39:26 +0200

    Add Coord.nextWrap

--- puzzles/src/main/scala/aoc/cartesian/Coord.scala
+++ puzzles/src/main/scala/aoc/cartesian/Coord.scala
@@ -5,11 +5,24 @@ case class Coord(cs: CoordSpace, idx: Int) {
   def c: Int = idx % cs.w
   def rc: (Int, Int) = (r, c)
 
-  def next(dir: Direction): Option[Coord] = dir match {
-    case Direction.N => Option.when(idx >= cs.w)(copy(idx = idx - cs.w))
-    case Direction.S => Option.when((idx + cs.w) < cs.size)(copy(idx = idx + cs.w))
-    case Direction.W => Option.when((idx % cs.w) != 0)(copy(idx = idx - 1))
-    case Direction.E => Option.when(((idx + 1) % cs.w) != 0)(copy(idx = idx + 1))
+  def next(dir: Direction): Option[Coord] = nextWrap(dir).toOption
+
+  def nextWrap(dir: Direction): Either[Coord, Coord] = dir match {
+    case Direction.N =>
+      val i = idx - cs.w
+      if (i < 0) Left(copy(idx = cs.size + i)) else Right(copy(idx = i))
+
+    case Direction.S =>
+      val i = idx + cs.w
+      if (i >= cs.size) Left(copy(idx = i - cs.size)) else Right(copy(idx = i))
+
+    case Direction.W =>
+      val i = idx - 1
+      if (idx % cs.w == 0) Left(copy(idx = i + cs.w)) else Right(copy(idx = i))
+
+    case Direction.E =>
+      val i = idx + 1
+      if (i % cs.w == 0) Left(copy(idx = i - cs.w)) else Right(copy(idx = i))
   }
 
   def manhattanDistance(that: Coord): Int = (this.r - that.r).abs + (this.c - that.c).abs
