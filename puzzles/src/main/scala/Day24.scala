f2025dc2331e7e8f8e1f09a043c2dccb9b61716f
Maksim Lanin <max.m.lanin@gmail.com>
2023-12-24 10:06:14 +0200

    Day24 part1

--- puzzles/src/main/scala/Day24.scala
+++ puzzles/src/main/scala/Day24.scala
@@ -8,9 +8,9 @@ import kyo.tries.Tries
 object Day24 extends util.AocApp(2023, 24) {
   case class Input(hail: List[Stone], testArea: TestArea)
 
-  case class Stone(px: Long, py: Long, pz: Long, vx: Long, vy: Long, vz: Long)
+  case class Stone(px: BigDecimal, py: BigDecimal, pz: BigDecimal, vx: BigDecimal, vy: BigDecimal, vz: BigDecimal)
 
-  case class TestArea(min: Long, max: Long)
+  case class TestArea(min: BigDecimal, max: BigDecimal)
 
   object Input {
     given Show[Input] = i => s"Hail:\n  ${i.hail.mkString("\n  ")}\n${i.testArea}"
@@ -19,9 +19,9 @@ object Day24 extends util.AocApp(2023, 24) {
   def parseInput(s: String): Input > Effects = {
     import cats.parse.{Numbers as N, Rfc5234 as R, Parser as P}
 
-    val long    = N.signedIntString.map(_.toLong)
-    val longSep = long <* P.charIn(",@ ").rep0.?
-    val stone   = (longSep, longSep, longSep, longSep, longSep, long).mapN(Stone.apply)
+    val n       = N.signedIntString.map(BigDecimal(_))
+    val longSep = n <* P.charIn(",@ ").rep0.?
+    val stone   = (longSep, longSep, longSep, longSep, longSep, n).mapN(Stone.apply)
     val hail    = stone.repSep(R.lf).map(_.toList)
 
     val testArea = P.string("test area ") *> (longSep, longSep).mapN(TestArea.apply)
@@ -37,24 +37,25 @@ object Day24 extends util.AocApp(2023, 24) {
     def cross(s1: Stone, s2: Stone) = {
       // x = ((vy1 * vx2 * px1) + (vx1 * vx2 * py2) - (vx1 * vx2 * py1) - (vx1 * vy2 * px2)) / (vy1 * vx2 - vx1 * vy2)
       // format: off
-      val x = ((s1.vy * s2.vx * s1.px) + (s1.vx * s2.vx * s2.py) - (s1.vx * s2.vx * s1.py) - (s1.vx * s2.vy * s2.px)) / (s1.vy * s2.vx - s1.vx * s2.vy).toDouble
-      val y = ((s1.vx * s2.vy * s1.py) + (s1.vy * s2.vy * s2.px) - (s1.vy * s2.vy * s1.px) - (s1.vy * s2.vx * s2.py)) / (s1.vx * s2.vy - s1.vy * s2.vx).toDouble
+      val x = (s1.vy * s2.vx - s1.vx * s2.vy).some.filter(_ != 0).map(d => ((s1.vy * s2.vx * s1.px) + (s1.vx * s2.vx * s2.py) - (s1.vx * s2.vx * s1.py) - (s1.vx * s2.vy * s2.px)) / d)
+      val y = (s1.vx * s2.vy - s1.vy * s2.vx).some.filter(_ != 0).map(d => ((s1.vx * s2.vy * s1.py) + (s1.vy * s2.vy * s2.px) - (s1.vy * s2.vy * s1.px) - (s1.vy * s2.vx * s2.py)) / d)
       // format: on
-      (x, y)
+      (x, y).tupled
     }
     val result = input.hail.combinations(2).count { s1_s2 =>
       val List(s1, s2) = s1_s2
-      val xy @ (x, y)  = cross(s1, s2)
-      def inTestArea =
-        x >= input.testArea.min && x <= input.testArea.max && y >= input.testArea.min && y <= input.testArea.max
-      def inFuture =
-        x >= s1.px * s1.vx.sign && y >= s1.py * s1.vy.sign && x >= s2.px * s2.vx.sign && y >= s2.py * s2.vy.sign
-      val result = inTestArea && inFuture
-      // println(s"$result: cross($s1, $s2) = $xy")
+      val result = cross(s1, s2).exists { (x, y) =>
+        def inTestArea =
+          x >= input.testArea.min && x <= input.testArea.max && y >= input.testArea.min && y <= input.testArea.max
+
+        def inFuture =
+          (s1.vx.sign == (x - s1.px).sign) && (s1.vy.sign == (y - s1.py).sign) &&
+            (s2.vx.sign == (x - s2.px).sign) && (s2.vy.sign == (y - s2.py).sign)
+
+        inTestArea && inFuture
+      }
       result
     }
-
-    // assert(result > 9277)
     s"$result"
   }
 
